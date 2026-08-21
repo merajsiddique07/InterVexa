e464b10c9a61cb8ca7f9c3ccdea1ada63e8e44a6
Meraj Siddique <merajsiddique9628@gmail.com>
2026-08-21 16:25:20 +0530

    client url updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://intervexa-ai-client-yvdj.onrender.com",
     credentials: true,
   }),
 );
